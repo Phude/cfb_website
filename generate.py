033af1b1f3b97eb8439b3b6ca03fad284f505bc9
kevin <kevin@moo.local>
2018-01-10 19:34:21 -0700

    initial commit

---
+++ generate.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+import jinja2
+
+template_path = 'templates'
+output_path = 'output/'
+static_path = 'static/'
+
+def render(filename, context):
+	return jinja2.Environment(
+		loader=jinja2.FileSystemLoader(template_path)
+	).get_template(filename).render(context)
+
+context = {
+	'test': 't'
+}
+
+if os.path.exists(output_path):
+	shutil.rmtree(output_path)
+
+shutil.copytree(static_path, output_path)
+
+render('about.html', context)
+
+for entry in os.scandir(template_path):
+	with open(output_path + entry.name, 'w+') as f:
+		if entry.name.endswith('.html'):
+			f.write(render(entry.name, context))
